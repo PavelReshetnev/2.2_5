9c361510ed593eef0a1e68d50033a378b386d7f2
PavelReshetnev <pasha8804088@gmail.com>
2023-03-04 11:53:14 +0700

    Initial commit

---
+++ ConsoleApp5/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp5;
+
+public class Person
+{
+
+    private int num1;
+    private int num2;
+
+    public int Num1
+    {
+        get => this.num1;
+        set => this.num1 = value;
+    }
+
+    public int Num2
+    {
+        get => this.num2; 
+        set => this.num2 = value;
+    }
+
+    public Person(int num1, int num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public Person()
+    {
+        Num1 = 222;
+        Num2 = 333;
+    }
+
+
+
+    ~Person()
+    {
+        Console.WriteLine($"{Num1} has been deleted");
+        Console.WriteLine($"{Num2} has been deleted");
+    }
+}
+
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp5;
+
+public class Program
+{
+    public static void Method()
+    {
+        Person tom = new Person();
+    }
+    static void Main(string[] args)
+    {
+        Method();
+
+        GC.Collect();
+        Console.ReadLine();
+    }
+}
